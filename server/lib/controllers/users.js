f479a05a37804fd7899df736d01edbdb125af4a1
Dave Fisher <dave.j.fish@gmail.com>
2022-11-10 10:52:49 -0800

    basic file structure for backend laid out

---
+++ server/lib/controllers/users.js
@@ -0,0 +1,16 @@
+const { Router } = require('express');
+const authenticate = require('../middleware/authenticate');
+const User = require('../models/users');
+const UserService = require('../services/UserService');
+
+const ONE_DAY_IN_MS = 1000 * 24 * 24 * 60;
+
+module.exports = Router()
+  .post('/sessions', async (req, res, next) => {
+    try {
+      // write out users
+    } catch (err) {
+      next(err)
+    }
+
+  });

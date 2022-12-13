fef5a316c57974d77a0b191713f40a8b21bb5f11
Dave Fisher <dave.j.fish@gmail.com>
2022-12-12 17:58:32 -0800

    added animation to target sound

--- client/components/TargetSound/TargetSound.js
+++ client/components/TargetSound/TargetSound.js
@@ -1,9 +1,21 @@
-
+import { motion } from 'framer-motion';
 
 export default function TargetSound({ targetSound }) {
   return (
-    <h2 className={'title'}>
-      {targetSound}
-    </h2>
+    <motion.div
+      animate={{
+        scale: [1, .9, 1.1, .9, 1]
+      }}
+      transition={{
+        duration: 2,
+        ease: 'easeInOut',
+        repeat: Infinity,
+        repeatDelay: 2,
+      }}>
+      <h2 className={'title'}>
+        {targetSound}
+      </h2>
+    </motion.div>
+    
   );
 }
